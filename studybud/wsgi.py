4127a2537023262b9cf450f38174cc65dbcd2427
avins <avins@umich.edu>
2023-08-25 13:38:45 -0400

    vercel config

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studybud.settings")
 
 application = get_wsgi_application()
+
+# For vercel deployment
+app = application
